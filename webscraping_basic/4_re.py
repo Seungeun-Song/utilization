93e32569462e0d9a9f415c471a583762d4bd5b75
Seungeun_Song <lilybad88@gmail.com>
2021-02-17 20:39:35 +0900

    Modified 4_re.py

--- webscraping_basic/4_re.py
+++ webscraping_basic/4_re.py
@@ -1,4 +1,4 @@
-# 정규식 
+# 정규식 (regular express, 간단히 regex)
 
 '''주민등록번호
 901201-111111 (o)
@@ -27,7 +27,7 @@ p = re.compile("ca.e")
 # $ (se$) : 문자열의 끝 > case, base(o) | face(x)
 
 '''m = p.match("case")  # patter(P)와 매치가 되는지
-print(m.group())  # 매치되지 않으면 에러가 발생'''
+print(m.group())  # 매치되지 않으면 에러가 발생/ group 메서드는 정규식 전체의 일치부를 찾는다.'''
 
 
 '''n = p.match("caffe")
@@ -67,7 +67,7 @@ lst = p.findall("good care cafe")
 print(lst)'''
 
 
-1. p = re.compile("원하는 형태의 정규식")
+'''1. p = re.compile("원하는 형태의 정규식")
 2. m = p.match("비교할 문자열") : 주어진 문자열의 처음부터 일치하는지 확인 
 3. m = p.search("비교할 문자열") : 주어진 문자열 중에 일치하는 게 있는지 확인
 4. lst = p.findall("비교할 문자열") : 일치하는 모든 것을 *리스트* 형태로 반환
@@ -75,4 +75,4 @@ print(lst)'''
 원하는 형태 : 정규식
  . (ca.e): 하나의 문자를 의미 > care, cafe, case (o)  | caffe(x)
  ^ (^de): 문자열의 시작 > dest, destination (o) | fade#p (x)
- $ (se$) : 문자열의 끝 > case, base(o) | face(x)
+ $ (se$) : 문자열의 끝 > case, base(o) | face(x)'''
