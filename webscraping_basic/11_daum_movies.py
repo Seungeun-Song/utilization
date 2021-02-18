c536ec5662873ec6a45f257b647b87608bbee880
Seungeun_Song <lilybad88@gmail.com>
2021-02-18 22:27:29 +0900

    Add 11_daum_movies.py

--- webscraping_basic/11_daum_movies.py
+++ webscraping_basic/11_daum_movies.py
@@ -1,18 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
-#for i 
-#url = "https://search.daum.net/search?w=tot&q={}%EB%85%84%EC%98%81%ED%99%94%EC%88%9C%EC%9C%84&DA=MOR&rtmaxcoll=MOR".format(i)
-res = requests.get("https://search.daum.net/search?w=tot&q=2019%EB%85%84%EC%98%81%ED%99%94%EC%88%9C%EC%9C%84&DA=MOR&rtmaxcoll=MOR")
-res.raise_for_status()
-soup = BeautifulSoup(res.text, "lxml")
 
-images = soup.find_all("img", attrs={"class":"thumb_img"})
 
-for image in images:
-    #print(image["src"])
-    image_url = image["src"]
-    if image_url.startswith("//"):
-        image_url = "http:" + image_url
 
-    print(image_url)
+for year in range(2015, 2020):
+    url = "https://search.daum.net/search?w=tot&q={}%EB%85%84%EC%98%81%ED%99%94%EC%88%9C%EC%9C%84&DA=MOR&rtmaxcoll=MOR".format(year)
+    res = requests.get(url)
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text, "lxml")
+
+    images = soup.find_all("img", attrs={"class":"thumb_img"})
+
+    for idx, image in enumerate(images):
+        #print(image["src"])
+        image_url = image["src"]
+        if image_url.startswith("//"):
+            image_url = "http:" + image_url
+
+        print(image_url)
+        
+        # image_url에 접속해서 이 페이지에 있는 정보를 파일로 저장하기 위해서 requests를 이용해서 새롭게 접속하는 것
+        image_res = requests.get(image_url)
+        image_res.raise_for_status()
+
+        with open("movie_{}_{}.jpg".format(year, idx+1), "wb") as f:
+            f.write(image_res.content)
+
+        if idx >= 4:  # 상위 5개 이미지까지만 다운로드
+            break
