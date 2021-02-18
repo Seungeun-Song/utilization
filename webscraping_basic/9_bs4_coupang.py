6c041a2043b0b48c50928ebb20278d8971aec39c
Seungeun_Song <lilybad88@gmail.com>
2021-02-18 22:26:54 +0900

    Modified 9_bs4_coupang.py

--- webscraping_basic/9_bs4_coupang.py
+++ webscraping_basic/9_bs4_coupang.py
@@ -3,7 +3,7 @@ import re
 from bs4 import BeautifulSoup
 
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"}
-url = "https://www.coupang.com/np/search?q=%EB%85%B8%ED%8A%B8%EB%B6%81&channel=user&component=&eventCategory=SRP&trcid=&traid=&sorter=scoreDesc&minPrice=&maxPrice=&priceRange=&filterType=&listSize=36&filter=&isPriceRange=false&brand=&offerCondition=&rating=0&page=3&rocketAll=false&searchIndexingToken=1=4&backgroundColor="
+url = "https://www.coupang.com/np/search?q=%EB%85%B8%ED%8A%B8%EB%B6%81&channel=user&component=&eventCategory=SRP&trcid=&traid=&sorter=scoreDesc&minPrice=&maxPrice=&priceRange=&filterType=&listSize=36&filter=&isPriceRange=false&brand=&offerCondition=&rating=0&page=1&rocketAll=false&searchIndexingToken=1=4&backgroundColor="
 res = requests.get(url, headers = headers)
 res.raise_for_status()
 soup = BeautifulSoup(res.text, "lxml")
