4b0b79a6abd9166ea4e05133ea24639dfb7ee09d
Seungeun_Song <lilybad88@gmail.com>
2021-02-19 19:53:47 +0900

    write to csv_ 12_csv_stock.py

--- webscraping_basic/12_csv_stock.py
+++ webscraping_basic/12_csv_stock.py
@@ -4,7 +4,15 @@ from bs4 import BeautifulSoup
 
 url = "https://finance.naver.com/sise/sise_market_sum.nhn?sosok=0&page="
 
-for page in range(1,2):
+filename = "시가총액1-200.csv"
+f = open(filename, "w", encoding="utf-8-sig", newline="")
+writer = csv.writer(f)
+
+title = "N	종목명	현재가	전일비	등락률	액면가	시가총액	상장주식수	외국인비율	거래량	PER	ROE	토론실".split("\t")
+print(type(title))
+writer.writerow(title)
+
+for page in range(1,5):
     res = requests.get(url + str(page))
     res.raise_for_status()
     soup = BeautifulSoup(res.text, "lxml")
@@ -15,4 +23,5 @@ for page in range(1,2):
         if len(columns) <= 1:  # 의미없는 데이터는 skip
             continue
         data = [column.get_text().strip() for column in columns]
-        print(data)
+        #print(data)
+        writer.writerow(data)
