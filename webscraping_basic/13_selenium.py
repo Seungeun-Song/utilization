efcfb98e10a8fa538d068f6e4bd99087edd690a3
Seungeun_Song <lilybad88@gmail.com>
2021-02-19 20:27:47 +0900

    search with selenium_ 13_selenium.py

--- webscraping_basic/13_selenium.py
+++ webscraping_basic/13_selenium.py
@@ -20,3 +20,28 @@ elem
 from selenium.webdriver.common.keys import Keys  # keys.enter을 위해. 즉 엔터를 치기 위해서. 검색버튼을 find할 거면 필요X
 elem.send_keys("나도코딩")
 elem.send_keys(Keys.ENTER)
+
+elem = browser.find_elements_by_tag_name("a")
+elem
+
+for e in elem:
+    e.get_attribute("href")  #href 전부 찾기
+    # 또 엔터 누르면 출력
+
+browser.get("http://daum.net")
+
+elem = browser.find_element_by_name("q")
+elem.send_keys("나도코딩")
+elem.send_keys(Keys.ENTER)
+browser.back()
+
+elem = browser.find_element_by_name("q")
+elem.send_keys("나도코딩")
+elem = browser.find_element_by_xpath('//*[@id="daumSearch"]/fieldset/div/div/button[2]') # xpath 이용
+elem
+elem.click()
+
+browser.close()  # 브라우저 창 닫기
+browser.quit()   # 브라우저 전부 닫기
+
+exit() # 파이썬 종료
