3e48f14ce215ef4dad45d6f575faca57300da3b7
Seungeun_Song <lilybad88@gmail.com>
2021-02-22 21:33:49 +0900

    Modified 8_page_scroll.py

--- webscraping_basic/rpa_basic/3.web/8_page_scroll.py
+++ webscraping_basic/rpa_basic/3.web/8_page_scroll.py
@@ -23,8 +23,32 @@ elem.send_keys(Keys.ENTER)
 # browser.execute_script('window.scrollTo(0,2080)') 
 
 # 화면 가장 아래로 스크롤 내리기
-browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+#browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
 
+# 동적  페이지에 대해서 마지막 스크롤 반복 수행
+interval = 1 # 2초에 한번씩 스크롤 내리기
+
+# 현재 문서 높이 가져와서 저장
+prev_height = browser.execute_script('return document.body.scrollHeight')
+
+# 반복 수행
+while True:
+    # 스크롤을 화면 가장 아래로 내림
+    browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+
+    # 페이지 로딩 대기
+    time.sleep(interval)
+
+    # 현재 문서 높이 가져와서 저장
+    curr_height = browser.execute_script('return document.body.scrollHeight')
+    if curr_height == prev_height: # 직전 높이와 같으면, 높이 변화가 없으면
+        break # 반복문 탈출(모든 스크롤 동작 완료)
+    
+    prev_height = curr_height
+
+time.sleep(2)
+# 맨 위로 올리기
+browser.execute_script('window.scrollTo(0,0)') 
 
 time.sleep(5)
 browser.quit()
